b528c67a4c6d2ea06c6496ad4257cbcac3d5a70c
nohjunh <nsh042777@naver.com>
2022-09-03 15:32:58 +0900

    using JS in JSX

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const product = "macbook";
+const size= "Air";
+const imageUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png';
+
+function handleClick(e){
+  alert('곧 도착합니다.');
+}
 root.render(
   <>
-    <p>안녕</p>
-    <p>리액트!</p>
+    <h1>{product+' '+size} 주문하기</h1>
+    <img src={imageUrl} alt="제품 사진" />
+    <button onClick={handleClick}>확인</button>
   </>
 );
