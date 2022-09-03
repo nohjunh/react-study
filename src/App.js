3234491df4e3cd89115e8abf31ba3185bad4e170
nohjunh <nsh042777@naver.com>
2022-09-03 16:58:07 +0900

    Props_basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Dice from './Dice';
+import Dice from './Dice'
 
-function App(){
-  return(
+function App() {
+  return ( // props = 컴포넌트에 속성값 넣는 방법.
     <div>
-      <Dice />
+      <Dice color="red" num={2} />  
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/HandIcon.js
@@ -0,0 +1,11 @@
+import imaGe from './assets/rock.svg';
+
+function HandIcon(){
+  return(
+    <>
+      <img src={imaGe} alt="이미지"/>
+    </>
+  )
+}
+
+export default HandIcon;
