0f38b230c591b7791c46e82cef169e005d637367
nohjunh <nsh042777@naver.com>
2022-09-03 15:24:04 +0900

    프래그먼트

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+root.render(
+  <>
+    <p>안녕</p>
+    <p>리액트!</p>
+  </>
+);
