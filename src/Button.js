f116f89d702e42e6c6674ff534c98175eb8dd741
nohjunh <nsh042777@naver.com>
2022-09-04 15:32:41 +0900

    rockscissorpaer_inline style_basic

--- src/Button.js
+++ src/Button.js
@@ -2,8 +2,35 @@
 // destructuring으로 객체요소를 바로 빼서 사용한다.
 // props는 묶여서 객체로 전달되기 때문
 
-function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
+const baseButtonStyle = {
+  padding: '14px 27px',
+  outline: 'none',
+  cursor: 'pointer',
+  borderRadius: '9999px',
+  fontSize: '17px',
+};
+
+const blueButtonStyle={
+  ...baseButtonStyle,
+  border: 'slolid 1px #7090ff',
+  color: '#7090ff',
+  backgroundColor: 'rgba(0, 89, 255, 0.2)',
+};
+
+const redButtonStyle={
+  ...baseButtonStyle,
+  border: 'slolid 1px #ff4664',
+  color: '#ff4664',
+  backgroundColor: 'rgba(255, 78, 78, 0.2)',
+};
+
+function Button({ color, children, onClick }) {
+  const style = color==='red' ? redButtonStyle : blueButtonStyle;
+  return (
+    <button style={style} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/HandButton.js
+++ src/HandButton.js
@@ -1,4 +1,19 @@
-import HandIcon from "./HandIcon";
+import HandIcon from './HandIcon';
+import backgroundImg from './assets/purple.svg';
+
+const style = {
+  width: '166px',
+  height: '166px',
+  border: 'none',
+  outline: 'none',
+  textAlign: 'center',
+  cursor: 'pointer',
+  backgroundColor: 'transparent',
+  backgroundImage: `url('${backgroundImg}')`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'contain',
+};
 
 // 여기서의 onClick함수는 상위컴포넌트 App의 handleButtonClick함수
 function HandButton({value, onClick}){
@@ -7,7 +22,7 @@ function HandButton({value, onClick}){
   // 여기서 value 값(아규먼트)은 handleButtonClick 함수 안에서는 nextHand 파라미터로 전달된다.
   const handleClick = () => onClick(value);
   return (
-    <button onClick={handleClick}>
+    <button style={style} onClick={handleClick}>
       <HandIcon value={value} />
     </button>
   );
