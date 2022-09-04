89269e75504b4959f145cdd28e557ff14c9a37a3
nohjunh <nsh042777@naver.com>
2022-09-04 13:35:39 +0900

    rockscissorpaper_record results

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import Button from './Button';
 import HandButton from './HandButton';
 import HandIcon from './HandIcon';
 import { compareHand, generateRandomHand } from './utils';
-import { useState } from 'react';
+
+const INITIAL_VALUE = 'rock';
 
 function getResult(me, other) {
   const comparison = compareHand(me, other);
@@ -12,21 +14,28 @@ function getResult(me, other) {
 }
 
 function App() {
-  // hand와 otherHand를 state로 바꾸어 주세요
-  const [hand, setHand] = useState('rock');
-  const [otherHand, setOtherHand] = useState('rock');
+  const [hand, setHand] = useState(INITIAL_VALUE);
+  const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+  // 배열을 통해 승부를 기록하는 기능을 구현
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleButtonClick = (nextHand) => {
-    // hand의 값을 nextHand 로 바꿔주세요
-    // otherHand의 값을 generateRandomHand()의 리턴 값으로 바꿔주세요
+    const nextOtherHand = generateRandomHand();
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
     setHand(nextHand);
-    setOtherHand( generateRandomHand() );
+    setOtherHand(nextOtherHand);
+    // gameHistory에 nextHistoryItem 을 추가해주세요.
+    // 배열은 참조형이기에 push로 값이 추가된다해도 주소값은 그대로이다.
+    // 따라서 gameHistory 자체의 값이 바뀐게 아니기에 useState로도 자동으로 렌더링이 되지 않는다. 
+    // 즉, gameHistory 는 참조형이니까, setGameHistory 함수로 값을 변경할 때는 매번 새로운 값을 만들어 주어야 합니다.
+    setGameHistory([...gameHistory, nextHistoryItem]);
   };
 
   const handleClearClick = () => {
-    // hand와 otherHand의 값을 'rock' 으로 변경해주세요
-    setHand('rock');
-    setOtherHand('rock');
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+    // gameHistory를 비워주세요
+    setGameHistory([]);
   };
 
   return (
@@ -38,7 +47,11 @@ function App() {
         VS
         <HandIcon value={otherHand} />
       </div>
+      <p>승부 기록: {gameHistory.join(', ')}</p>
       <div>
+        {/* 1. 사용자가 버튼을 클릭함*/}
+        {/* 2. HandButton 컴포넌트의 handleClick이 실행됨: handleClick(e) 이렇게 이벤트 객체가 전달되면서 실행이 될겁니다*/}
+        {/* 2. 상위 컴포넌트인 App에서 onClick함수인 handleButtonClick을 HandButton 컴포넌트에게 Prop으로 내려준다. */}
         <HandButton value="rock" onClick={handleButtonClick} />
         <HandButton value="scissor" onClick={handleButtonClick} />
         <HandButton value="paper" onClick={handleButtonClick} />
--- src/HandButton.js
+++ src/HandButton.js
@@ -1,6 +1,10 @@
 import HandIcon from "./HandIcon";
 
+// 여기서의 onClick함수는 상위컴포넌트 App의 handleButtonClick함수
 function HandButton({value, onClick}){
+  // 3. handleClick 함수에서는 onClick함수를 value값으로실행합니다.
+  // 즉, 다시말해서 onClick(value)===handleButtonClick(value)가 실행되는 것입니다
+  // 여기서 value 값(아규먼트)은 handleButtonClick 함수 안에서는 nextHand 파라미터로 전달된다.
   const handleClick = () => onClick(value);
   return (
     <button onClick={handleClick}>
